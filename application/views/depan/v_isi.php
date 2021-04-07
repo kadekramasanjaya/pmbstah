8d920831d8bdae37a56ec12d6d0f85250497dd44
ramskuy <ramasanjayakd@gmail.com>
2021-04-07 15:43:42 +0800

    revisi

---
+++ application/views/depan/v_isi.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PMB STAH N MPU KUTURAN</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/template/img/stahlogo.png" rel="icon">
+  <link href="assets/template/img/stahlogo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/template/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/template/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/template/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/template/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/template/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/template/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Techie - v2.0.0
+  * Template URL: https://bootstrapmade.com/techie-free-skin-bootstrap-3/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-9 d-flex align-items-center">
+          <h1 class="logo mr-auto"><a href="index.html">PMB STAH N MPU KUTURAN</a></h1>
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+          <nav class="nav-menu d-none d-lg-block">
+            <ul>
+              <!-- <li class="active"><a href="index.html">Home</a></li> -->
+            </ul>
+          </nav><!-- .nav-menu -->
+          <a href="#" class="get-started-btn scrollto">Kembali</a>
+        </div>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container-flsuid" data-aos="fade-up">
+      <div class="row justify-content-center">
+        <div class="hero-img" data-aos="zoom-in" data-aos-delay="150">
+          <img src="assets/template/img/pmbstah.png" class="img-fluid animated" alt="">
+        </div>
+        <div class="col-l-3 col-lg-6 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>SELAMAT DATANG MAHASISWA BARU STAHN MPU KUTURAN</h1>
+          <h2>Cari Nama Dibawah Ini</h2>
+
+          <!-- Search Bar -->
+          <div class="input-group">
+            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" s
+              aria-describedby="search-addon" />
+            <!-- Button trigger modal -->
+            <a class="btn btn-primary" href="#">CARI</a>
+            <!-- Modal -->
+            <!-- Penutup Search Bar -->
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <!-- End Hero -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/template/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/template/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/template/vendor/php-email-form/validate.js"></script>
+  <script src="assets/template/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/template/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/template/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/template/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/template/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/template/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/template/js/main.js"></script>
+
+</body>
+
+</html>
